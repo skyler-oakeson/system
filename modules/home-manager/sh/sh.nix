c70c1e9400ad26c0c9c0b9d729e0eb4262a649a6
skyler-oakeson <skyler.oakeson@gmail.com>
2024-12-15 17:33:13 -0700

    73 current  2024-12-15 17:32:26  25.05.20241211.5d67ea6  6.6.64                          *

--- modules/home-manager/sh/sh.nix
+++ modules/home-manager/sh/sh.nix
@@ -11,17 +11,17 @@
 
         git add --all
 
-        # Early return if no changes
-        # if git diff --quiet '*.nix'; then
-        #     echo "No changes detected, exiting."
-        #     popd
-        #     exit 0
-        # fi
+        Early return if no changes
+        if git diff --quiet '*.nix'; then
+            echo "No changes detected, exiting."
+            popd
+            exit 0
+        fi
 
         # Autoformat your nix files
         # alejandra . &>/dev/null || ( alejandra . ; echo "formatting failed!" && exit 1)
 
-        # git diff -U0 '*.nix
+        git diff -U0 '*.nix
 
         echo "NixOS Rebuilding..."
 
@@ -32,9 +32,6 @@
         current=$(nixos-rebuild list-generations | grep current)
 
         git commit -am "$current"
-
-        notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
-        exit 1
         '')
   ];
 
