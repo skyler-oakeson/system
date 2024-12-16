51a827d01d37eb35056ba2874be708b5c04996b6
skyler-oakeson <skyler.oakeson@gmail.com>
2024-12-15 17:04:54 -0700

    Rebuild.

--- modules/home-manager/sh/sh.nix
+++ modules/home-manager/sh/sh.nix
@@ -4,10 +4,10 @@
   home.packages = with pkgs; [
       zsh
       (pkgs.writeShellScriptBin "rebuild.sh" ''
-        pushd ~/.config/system/
+        pushd /home/skyler/.config/system
         git add --all
 
-        # Early return if no changes
+        Early return if no changes
         if git diff --quiet '*.nix'; then
             echo "No changes detected, exiting."
             popd
@@ -17,12 +17,12 @@
         # Autoformat your nix files
         alejandra . &>/dev/null || ( alejandra . ; echo "formatting failed!" && exit 1)
 
-        git diff -U0 '*.nix
+        # git diff -U0 '*.nix
 
         echo "NixOS Rebuilding..."
 
         # Rebuild, output simplified errors, log trackebacks, if error find the errors print them and return
-        sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+        # sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
 
         # Get current generation metadata
         current=$(nixos-rebuild list-generations | grep current)
