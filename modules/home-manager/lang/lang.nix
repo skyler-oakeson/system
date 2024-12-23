351d8b6e551db96f732f474c6d694335d6965fcf
skyler-oakeson <skyler.oakeson@gmail.com>
2024-12-22 17:14:03 -0700

    100 current  2024-12-22 17:13:58  25.05.20241211.5d67ea6  6.6.64                          *

--- modules/home-manager/lang/lang.nix
+++ modules/home-manager/lang/lang.nix
@@ -1,15 +1,22 @@
 { pkgs, ... }: 
 
 # ===============================================
-# Installed Programming Languages
+# Installed Compilers, LSP's, and Formatters
 # 
 # Search: https://search.nixos.org for more pkgs 
 # ===============================================
 
 {
   home.packages = with pkgs; [ 
+    # Compilers 
     ghc
     python39
     gcc
+
+    # LSPs
+    nil
+
+    # Formatters
+    alejandra
   ];
 }
