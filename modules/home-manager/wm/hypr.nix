65b481cec32a2a84968b996f48ea8bcc41db6178
skyler-oakeson <skyler.oakeson@gmail.com>
2024-12-23 16:45:27 -0700

    110 current  2024-12-23 16:43:48  25.05.20241219.d70bd19  6.6.66                          *

--- modules/home-manager/wm/hypr.nix
+++ modules/home-manager/wm/hypr.nix
@@ -28,17 +28,17 @@
             preserve_split = "yes";
         };
 
-        env = [
-            "MOZ_ENABLE_WAYLAND,1"
-            "XCURSOR_SIZE,24"
-            "QT_QPA_PLATFORMTHEME,qt6ct"
-            "WALLPAPER_PATH, /home/skyler/Pictures/red_distortion.jpg"
-            "QT_QPA_PLATFORM,wayland;xcb"
-            "QT_QPA_PLATFORMTHEME,qt6ct"
-            "QT_QPA_PLATFORMTHEME,qt5ct"
-            "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-            "QT_AUTO_SCREEN_SCALE_FACTOR,1"
-        ];
+        # env = [
+        #     "MOZ_ENABLE_WAYLAND,1"
+        #     "XCURSOR_SIZE,24"
+        #     "QT_QPA_PLATFORMTHEME,qt6ct"
+        #     "WALLPAPER_PATH, /home/skyler/Pictures/red_distortion.jpg"
+        #     "QT_QPA_PLATFORM,wayland;xcb"
+        #     "QT_QPA_PLATFORMTHEME,qt6ct"
+        #     "QT_QPA_PLATFORMTHEME,qt5ct"
+        #     "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        #     "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        # ];
 
         "$terminal" = "kitty";
         "$fileManager" = "kitty ranger";
@@ -82,66 +82,66 @@
             ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ];
 
-        bind = [
-            "$mod, C, exec, $terminal"
-            "$mod, Q, killactive," 
-            "$mod, E, exec, $fileManager"
-            "$mod, V, togglefloating," 
-            "$mod, SPACE, exec, pkill wofi || $menu"
-
-            "$mod, U, pseudo, # dwindle"
-            "$mod, T, togglesplit, # dwindle"
-            "$mod, F, fullscreen, 0"
-
-            # Move focus with mod + vim keys
-            "$mod, H, movefocus, l"
-            "$mod, L, movefocus, r"
-            "$mod, K, movefocus, u"
-            "$mod, J, movefocus, d"
-
-            # Move windows with mod and shift + vim keys
-            "$mod Shift, H, movewindow, l"
-            "$mod Shift, L, movewindow, r"
-            "$mod Shift, K, movewindow, u"
-            "$mod Shift, J, movewindow, d"
-
-            # Switch workspaces with mod + [0-9]
-            "$mod, 1, workspace, 1"
-            "$mod, 2, workspace, 2"
-            "$mod, 3, workspace, 3"
-            "$mod, 4, workspace, 4"
-            "$mod, 5, workspace, 5"
-            "$mod, 6, workspace, 6"
-            "$mod, 7, workspace, 7"
-            "$mod, 8, workspace, 8"
-            "$mod, 9, workspace, 9"
-            "$mod, 0, workspace, 10"
-            "$mod, N, workspace, +1"
-            "$mod, P, workspace, -1"
-
-            # Move active window to a workspace with mod + SHIFT + [0-9]
-            "$mod SHIFT, 1, movetoworkspace, 1"
-            "$mod SHIFT, 2, movetoworkspace, 2"
-            "$mod SHIFT, 3, movetoworkspace, 3"
-            "$mod SHIFT, 4, movetoworkspace, 4"
-            "$mod SHIFT, 5, movetoworkspace, 5"
-            "$mod SHIFT, 6, movetoworkspace, 6"
-            "$mod SHIFT, 7, movetoworkspace, 7"
-            "$mod SHIFT, 8, movetoworkspace, 8"
-            "$mod SHIFT, 9, movetoworkspace, 9"
-            "$mod SHIFT, 0, movetoworkspace, 10"
-            "$mod SHIFT, N, movetoworkspace, +1"
-            "$mod SHIFT, P, movetoworkspace, -1"
-
-            # Example special workspace (scratchpad)
-            "$mod, S, togglespecialworkspace, magic"
-            "$mod SHIFT, S, movetoworkspace, special:magic"
-
-            # Scroll through existing workspaces with mod + scroll
-            "$mod, mouse_down, workspace, e+1"
-            "$mod, mouse_up, workspace, e-1"
-        ];
-    };
+        # bind = [
+        #     "$mod, C, exec, $terminal"
+        #     "$mod, Q, killactive," 
+        #     "$mod, E, exec, $fileManager"
+        #     "$mod, V, togglefloating," 
+        #     "$mod, SPACE, exec, pkill wofi || $menu"
+        #
+        #     "$mod, U, pseudo, # dwindle"
+        #     "$mod, T, togglesplit, # dwindle"
+        #     "$mod, F, fullscreen, 0"
+        #
+        #     # Move focus with mod + vim keys
+        #     "$mod, H, movefocus, l"
+        #     "$mod, L, movefocus, r"
+        #     "$mod, K, movefocus, u"
+        #     "$mod, J, movefocus, d"
+        #
+        #     # Move windows with mod and shift + vim keys
+        #     "$mod Shift, H, movewindow, l"
+        #     "$mod Shift, L, movewindow, r"
+        #     "$mod Shift, K, movewindow, u"
+        #     "$mod Shift, J, movewindow, d"
+        #
+        #     # Switch workspaces with mod + [0-9]
+        #     "$mod, 1, workspace, 1"
+        #     "$mod, 2, workspace, 2"
+        #     "$mod, 3, workspace, 3"
+        #     "$mod, 4, workspace, 4"
+        #     "$mod, 5, workspace, 5"
+        #     "$mod, 6, workspace, 6"
+        #     "$mod, 7, workspace, 7"
+        #     "$mod, 8, workspace, 8"
+        #     "$mod, 9, workspace, 9"
+        #     "$mod, 0, workspace, 10"
+        #     "$mod, N, workspace, +1"
+        #     "$mod, P, workspace, -1"
+        #
+        #     # Move active window to a workspace with mod + SHIFT + [0-9]
+        #     "$mod SHIFT, 1, movetoworkspace, 1"
+        #     "$mod SHIFT, 2, movetoworkspace, 2"
+        #     "$mod SHIFT, 3, movetoworkspace, 3"
+        #     "$mod SHIFT, 4, movetoworkspace, 4"
+        #     "$mod SHIFT, 5, movetoworkspace, 5"
+        #     "$mod SHIFT, 6, movetoworkspace, 6"
+        #     "$mod SHIFT, 7, movetoworkspace, 7"
+        #     "$mod SHIFT, 8, movetoworkspace, 8"
+        #     "$mod SHIFT, 9, movetoworkspace, 9"
+        #     "$mod SHIFT, 0, movetoworkspace, 10"
+        #     "$mod SHIFT, N, movetoworkspace, +1"
+        #     "$mod SHIFT, P, movetoworkspace, -1"
+        #
+        #     # Example special workspace (scratchpad)
+        #     "$mod, S, togglespecialworkspace, magic"
+        #     "$mod SHIFT, S, movetoworkspace, special:magic"
+        #
+        #     # Scroll through existing workspaces with mod + scroll
+        #     "$mod, mouse_down, workspace, e+1"
+        #     "$mod, mouse_up, workspace, e-1"
+        # ];
+      };
     };
 
     home.pointerCursor = {
