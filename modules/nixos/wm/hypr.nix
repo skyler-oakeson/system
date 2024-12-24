a2f48fd247d643f5fac9a99a96aaacfd2d57fb0f
skyler-oakeson <skyler.oakeson@gmail.com>
2024-12-24 14:25:51 -0700

    125 current  2024-12-24 14:25:43  25.05.20241219.d70bd19  6.6.66                          *

--- modules/nixos/wm/hypr.nix
+++ modules/nixos/wm/hypr.nix
@@ -5,7 +5,7 @@
     hyprutils
   ];
 
-  services.displayManager.sddm.enable = true; #This line enables sddm
+  # services.displayManager.sddm.enable = true; #This line enables sddm
   services.xserver.enable = true; # Might need this for Xwayland  
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; # This variable fixes electron apps in wayland
 
