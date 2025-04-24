5e188fd655ee31141fda70ae4163906f1fa599fd
skyler-oakeson <skyler.oakeson@gmail.com>
2025-04-24 12:04:14 -0600

    NixOS Gen := 159 current  2025-04-24 10:32:30  25.05.20250417.b024ced  6.12.23                          * home-manager Gen := 2025-04-24 11:58 : id 1038 -> /nix/store/7nsnzwqsrf92vqfhprk0pv399v8i9l3v-home-manager-generation

--- home-manager/apps/zsh.nix
+++ home-manager/apps/zsh.nix
@@ -64,7 +64,7 @@
       genNix=$(nixos-rebuild list-generations | grep current)
       genHM=$(home-manager generations | head -1)
 
-      sudo git commit -am "NixOS Gen := $genNix \n home-manager Gen := $genHM"
+      sudo git commit -am "Generation info \n NixOS Gen := $genNix \n home-manager Gen := $genHM"
       popd
     '')
   ];
